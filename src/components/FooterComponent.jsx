514b8ed0869f0945c13406a4e0b56cc63cc8798a
Jameel Ahmed <jameel.webdev@gmail.com>
2023-12-01 15:40:21 +0530

    Initiaziled Flight app frontend

---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FooterComponent = () => {
+  return <div>FooterComponent</div>;
+};
+
+export default FooterComponent;
---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HeaderComponent = () => {
+  return <div>HeaderComponent</div>;
+};
+
+export default HeaderComponent;
