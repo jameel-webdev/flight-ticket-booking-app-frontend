cb3dee1839051c44cc8222c3b2322afe6b515169
Jameel Ahmed <jameel.webdev@gmail.com>
2023-12-08 17:58:41 +0530

    store

--- src/slices/store.js
+++ src/slices/store.js
@@ -1,14 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./Users/userSlice";
-import flightReducer from "./Flights/flightSlice";
-import bookingReducer from "./Bookings/bookingSlice";
 import { apiSlice } from "./apiSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    flights: flightReducer,
-    bookings: bookingReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
