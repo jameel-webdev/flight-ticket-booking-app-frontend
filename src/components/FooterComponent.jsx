66e726e9c53d197ce50244535ad06d8830b9ca71
Jameel Ahmed <jameel.webdev@gmail.com>
2023-12-08 18:58:53 +0530

    reset fixed

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const FooterComponent = () => {
   return (
     <footer>
-      <Navbar bg="light" variant="light" expand="lg">
+      <Navbar bg="light" variant="light" expand="lg" fixed="bottom">
         <Container className="justify-content-center align-items-center">
           <Navbar.Brand>
             Copyright &copy;{" "}
